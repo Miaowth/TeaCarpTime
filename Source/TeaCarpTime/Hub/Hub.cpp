4166c8eff58d2106f75a9ef5bb7b5ce54d6b018d
George Britton <george.britton@student.nua.ac.uk>
2020-02-01 16:05:30 +0000

    Added screen logging of hud contents

--- Source/TeaCarpTime/Hub/Hub.cpp
+++ Source/TeaCarpTime/Hub/Hub.cpp
@@ -82,6 +82,13 @@ bool AHub::AddPart(AGenericPart* PartToAdd)
 		FVector DropPoint = UKismetMathLibrary::RandomPointInBoundingBox(HubZone->Box->GetComponentLocation(), HubZone->Box->GetScaledBoxExtent());
 		DropPoint.Z += 500;
 		PartToAdd->SetActorLocation(DropPoint);
+
+		for (int32 i = 0; i < ListOfCollectedParts.Num(); i++)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 2, FColor::Yellow, ListOfCollectedParts[i].Part->PartName.ToString());
+			GEngine->AddOnScreenDebugMessage(-1, 2, FColor::Yellow, FString::FromInt(ListOfCollectedParts[i].Quantity));
+		}
+
 		return true;
 	}
 	else { return false; }
